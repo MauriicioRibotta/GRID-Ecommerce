6ba6338ae2936b2aa8eda48bfba146472c35aa2d
Mauricio Ribotta <mauriribotta2@gmail.com>
2026-01-28 19:04:20 -0300

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiKey: 'AIzaSyDwcqPs59BKqODreBO4zF4nZLMWbgSTd_8',
-    supabaseUrl: 'https://axdmodidroscsjzbbxuw.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4ZG1vZGlkcm9zY3NqemJieHV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2MTA2NjMsImV4cCI6MjA4NTE4NjY2M30.I2l1ocMGvbtJrumQrJemHTKgRAOOs7FCHs-QgQ97png'
-};
